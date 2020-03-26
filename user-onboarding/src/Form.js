99bd85db141fd5b99646f1da885c1fa296bc88e8
Diddleslip <diddleslip@gmail.com>
2020-03-25 21:02:11 -0400

    Finished MVP

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
 import * as yup from "yup";
-import { axios } from "axios";
+import axios from "axios";
+
+const formSchema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    email: yup.string().email().required("Must include an email"),
+    password: yup.string().required("Password is required"),
+    terms: yup.boolean().oneOf([true], "Must sell your soul to continue")
+});
 
 export default function Form() {
+
+    const [buttonDisabled, setButtonDisabled] = useState (true);
+    const [post, setPost] = useState([]);
 
     const [formState, setFormState] = useState({
         name: "",
@@ -10,6 +20,21 @@ export default function Form() {
         password: "",
         terms: ""
     });
+
+    const [errors, setErrors] = useState({
+        name: "",
+        email: "",
+        password: "",
+        terms: ""
+    })
+
+    useEffect(() => {
+        formSchema.isValid(formState).then(valid => {
+            setButtonDisabled(!valid);
+        })
+    }, [formState])
+
+
 
     const inputChange = e => {
         e.persist();
@@ -20,11 +45,51 @@ export default function Form() {
             e.target.value
         };
         setFormState(newFormData);
+        validateChange(e)
+    }
+
+    const formSubmit = e => {
+        e.preventDefault();
+        axios
+          .post("https://reqres.in/api/users", formState)
+          .then(res => {
+            setPost(res.data);
+            console.log("success", post);
+    
+            setFormState({
+              name: "",
+              email: "",
+              password: "",
+              terms: "",
+            });
+          })
+          .catch(err => {
+            console.log(err.res);
+          });
+      };
+
+    const validateChange = e => {
+        yup
+        .reach(formSchema, e.target.name)
+        .validate(e.target.value)
+        .then(valid => {
+            setErrors({
+                ...errors,
+                [e.target.name]: ""
+            });
+        })
+        .catch(err => {
+            setErrors({
+                ...errors,
+                [e.target.name]: err.errors
+            })
+        })
     }
 
     return (
         <div>
-            <form>
+            <h2>Sign Up Registration</h2>
+            <form onSubmit={formSubmit}>
                 <label htmlFor="name">
                 Name:
                     <input
@@ -55,17 +120,18 @@ export default function Form() {
                         onChange= {inputChange}
                     />
                 </label><br/>
-                <label htmlfor="terms">
+                <label htmlFor="terms">
                 Terms of Service:
                     <input
                         type="checkbox"
                         id="terms"
                         name="terms"
-                        value={formState.terms}
+                        checked={formState.terms}
                         onChange= {inputChange}
                     />
                 </label><br/>
-                <button>Submit</button>
+                <pre>{JSON.stringify(post, null, 2)}</pre>
+                <button disabled={buttonDisabled}>Submit</button>
             </form>
         </div>
         )
